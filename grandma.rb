86e1362759726160b5e0036489fb32b34336d74d
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-06 02:13:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase.downcase 
-    return "HUH?! SPEAK UP, SONNY!"
-    else phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  end 
-  end 
++def speak_to_grandma(phrase)
++  if(phrase=="I LOVE YOU GRANDMA!")
++    return "I LOVE YOU TOO PUMPKIN!"
++  elsif (phrase=="#{phrase}".upcase)
++    return "NO, NOT SINCE 1938!"
++  else
++    return "HUH?! SPEAK UP, SONNY!"
++  end
++end
